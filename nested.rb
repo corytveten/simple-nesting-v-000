86b3e06e0568967b78034585b75a8efb101e62d7
corytveten <corytveten@gmail.com>
2018-06-29 18:13:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -199,3 +199,23 @@ def dennis_ritchies_language
 		programmer_hash[:dennis_ritchie][:languages].join
 
 end
+
+def adding_matz
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+	         :known_for => "Unix",
+	         :languages => ["C"]
+	       }
+	    }
+
+		programmer_hash = {yukihiro_matsumoto => {}}
+end
