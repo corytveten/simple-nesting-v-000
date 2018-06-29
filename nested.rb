ba722502da3529dd77b0be3b66287e4b51590e86
corytveten <corytveten@gmail.com>
2018-06-29 18:31:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -242,3 +242,24 @@ def changing_alan
 		programmer_hash
 
 end
+
+def adding_to_dennis
+	programmer_hash =
+		{
+				:grace_hopper => {
+					:known_for => "COBOL",
+					:languages => ["COBOL", "FORTRAN"]
+				},
+				:alan_kay => {
+					:known_for => "Object Orientation",
+					:languages => ["Smalltalk", "LISP"]
+				},
+				:dennis_ritchie => {
+					 :known_for => "Unix",
+					 :languages => ["C"]
+				 }
+			}
+
+	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash
+end
